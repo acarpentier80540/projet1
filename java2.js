92fd66f4be929f8fd7f43d3b14f488d860ab443b
antoine carpentier <a.carpentier.wcs@gmail.com>
2022-05-11 14:32:06 +0200

    nouvelle sep

--- java2.js
+++ java2.js
@@ -1,3 +1,22 @@
-let animation = anime({
-    target:
-}) 
+const valid =
+        document.getElementById('valid');
+
+const validation =
+        document.getElementById('validation');
+
+
+const closebtn =
+        document.getElementById('closebtn');
+
+
+
+            validation.addEventListener('click' , 
+            function() {
+
+                valid.setAttribute('open', true);
+            })
+
+            closebtn.addEventListener('click',
+            function () { 
+                
+            valid.setAttribute('open')} );

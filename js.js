21dff07319a85effa77b80a41ca266232079b2a0
antoine carpentier <a.carpentier.wcs@gmail.com>
2022-05-16 16:37:39 +0200

    finission 3

--- js.js
+++
@@ -1,9 +0,0 @@
-
-function closeMenu (){
-    const close = document.querySelector('.navbar__links');
-    const navbar = document.querySelector('.navbar');
-    close.addEventListener('click', (e) => {    
-      navbar.classList.toggle('show-nav');
-    });    
-  }
-  closeMenu()
